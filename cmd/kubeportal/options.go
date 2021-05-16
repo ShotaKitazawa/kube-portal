c550d755553783e9f705be64d81a65808191bf0e
ShotaKitazawa <skitazawa1121@gmail.com>
2021-05-16 17:46:23 +0900

    add someone for Docker

--- cmd/kubeportal/options.go
+++ cmd/kubeportal/options.go
@@ -1,13 +1,26 @@
 package kubeportal
 
-import "github.com/jessevdk/go-flags"
+import (
+	"fmt"
+	"os"
+
+	"github.com/jessevdk/go-flags"
+)
+
+var (
+	AppName    string
+	AppVersion string
+)
 
 type Opts struct {
+	Version func() `short:"v" long:"version" description:"show version"`
+
 	GitHubOAuthKey    string   `long:"github-key" description:"GitHub OAuth Client ID"`
 	GitHubOAuthSecret string   `long:"github-secret" description:"GitHub OAuth Client Secret"`
 	SessionSecret     string   `long:"session-secret" description:"OAuth session secret"`
 	GitHubAllowUsers  []string `short:"u" long:"github-allow-user" description:"GitHub Username that you allowed to get private links"`
 
+	// optional
 	KubeConfigPath string `long:"kubeconfig" description:"filepath of KubeConfig"`
 	BindAddr       string `long:"bind-addr" default:"0.0.0.0"`
 	BindPort       int    `long:"bind-port" default:"8080"`
@@ -15,8 +28,13 @@ type Opts struct {
 
 func Parse() (*Opts, error) {
 	var opts Opts
+	opts.Version = func() {
+		fmt.Printf("%s %s\n", AppName, AppVersion)
+		os.Exit(0)
+	}
 	if _, err := flags.Parse(&opts); err != nil {
 		return nil, err
 	}
+
 	return &opts, nil
 }
